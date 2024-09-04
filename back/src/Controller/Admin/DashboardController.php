0f96a08d0a958c8f6784fa26416a5506de8822bc
BONKANO Abdoul <bonkano69@gmail.com>
2024-09-04 09:54:15 +0200

    HomePage&Admin ok

---
+++ back/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+// src/Controller/Admin/DashboardController.php
+
+namespace App\Controller\Admin;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Garage V. Parrot');
+    }
+
+
+}
